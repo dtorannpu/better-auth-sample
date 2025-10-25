50b7bad9f628a803bf5ad2d0b8540f6e80ed04b9
dtorannpu <dtorannpu@gmail.com>
2025-10-25 14:50:04 +0900

    Prettier と ESLint の設定ファイルを追加し、依存関係を更新

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,6 +5,7 @@ import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
@@ -25,6 +26,7 @@ export default defineConfig([
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
   reactHooks.configs.flat.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.css"],
     plugins: { css },
